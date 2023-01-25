0d8207ef6740c8fed70a4f272781de9fd1c90e51
Farwa <farwa.mubasher@ryerson.ca>
2023-01-24 22:13:37 -0500

    authentication file so register and login

---
+++ routes/players.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { db, collection, getDocs } = require("../firebase");
+const { getPlayers } = require("../services/players.service");
+
+
+router.get("/", async (req,res) => {
+const players = await getPlayers(db)
+console.log("---", players)
+
+res.status(200).send({
+    status: "Success",
+    message: `Successfully retrieved players ${JSON.stringify(players)}`
+});
+
+})
+
+module.exports = router;
