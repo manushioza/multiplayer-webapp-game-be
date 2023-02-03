85dcb0f3c8c5ca13411c392a8eaf4877a0cd54c0
ishacheema <60581256+ishacheema@users.noreply.github.com>
2023-02-02 19:20:42 -0500

    adds auth backend apis login and register

---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const { db, where, query, collection, getDocs } = require("../firebase");
+const { getUsers, addUsers, login } = require("../services/users.service");
+
+router.get("/", async (req,res) => {
+  const users = await getUsers(db)
+  console.log("---", users)
+  
+  res.status(200).send({
+      status: "Success",
+      message: `Successfully retrieved users ${JSON.stringify(users)}`
+  });
+  })
+     
+router.post("/login", async function (req, res) {
+  const loginData = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  
+  try {
+    await login(db, loginData);
+    res.status(200).send();
+    console.log("Login successful")
+  } catch (err) {
+    console.log(err);
+    res.status(404).send({
+      status: "Error",
+      message: `Error Logging in: ${err}`,
+    });
+  }
+  });
+
+
+//Register
+router.post("/register", async function (req, res) {
+  const userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  try {
+    console.log("Attempting to add User to DB.....");
+    await addUsers(db, userData);
+    res.status(200).send({
+      status: "Success",
+      message: "Successfully added new User",
+    }); 
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      status: "Failed",
+      message: `Failed to add User. ${error}`,
+    });
+  }
+});
+  
+  module.exports = router;
